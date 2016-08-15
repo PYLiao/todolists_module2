4a4f75078a9adbd92590fdc7d914e7ddfff52e81
poyangliao <lby75429@gmail.com>
2016-08-15 10:41:18 +0800

    user_todolists

---
+++ db/migrate/20160815023856_create_todolists_users.rb
@@ -0,0 +1,7 @@
+class CreateTodolistsUsers < ActiveRecord::Migration
+  def change
+    create_table :todolists_users do |t|
+      t.references :user, index: true, foreign_key: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815022857) do
+ActiveRecord::Schema.define(version: 20160815023856) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20160815022857) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  create_table "todolists_users", force: :cascade do |t|
+    t.integer "user_id"
+  end
+
+  add_index "todolists_users", ["user_id"], name: "index_todolists_users_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
